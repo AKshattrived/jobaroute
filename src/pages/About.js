39e70f8d947506948ea1d1ddd7a502ce42fcc52d
AKshattrived <akshattrivedi7901@gmail.com>
2023-01-29 13:33:54 +0530

    React router before v6.4

---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ea
+        quisquam dolore repudiandae possimus. Reiciendis sit amet earum dolorum
+        praesentium rem distinctio voluptatibus nostrum, aliquam qui maxime
+        placeat dicta modi provident atque, velit quasi sapiente. Nisi ipsum
+        dolore suscipit eos error saepe rem, voluptas, dolores dolorum,
+        blanditiis molestiae eaque laborum?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, soluta
+        provident fugiat quasi consequatur ut sunt ratione facilis molestiae.
+        Fugit amet ex necessitatibus maxime natus ullam labore repudiandae
+        mollitia aliquid voluptatibus nesciunt voluptatem tempora quibusdam,
+        tenetur esse officia unde aperiam nobis, ratione qui vero nemo deleniti
+        earum! Rem, voluptates quos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero quasi
+        ipsa ratione voluptas eligendi delectus dolorum velit fugiat
+        perferendis, culpa fuga distinctio. Quia, tempore beatae!
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam esse
+        voluptatem aliquid enim! Ducimus, nihil? Possimus suscipit neque facilis
+        ipsa laborum ea sunt quos totam dolore quam cupiditate nemo voluptatem
+        voluptas tempora fugiat laboriosam, eos eaque aspernatur dolorum. In
+        soluta facere accusantium, culpa assumenda necessitatibus unde
+        repudiandae inventore nisi provident repellendus nemo ullam minus
+        tempora, fuga aperiam perferendis eligendi ipsa! Nisi, quam
+        consequuntur. Mollitia minus excepturi ratione libero in obcaecati quo
+        debitis itaque harum delectus totam, at hic a. Quis esse ducimus
+        impedit, consectetur porro voluptatum nesciunt error iste saepe
+        reprehenderit illum a mollitia nulla ipsum perferendis eveniet ipsam
+        delectus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, aut
+        repellendus voluptas similique alias est accusamus quasi doloremque
+        omnis maxime adipisci, molestias ad nesciunt ipsa nobis quis sed.
+        Corrupti optio minus, aperiam nemo perspiciatis illo! Error voluptatum
+        facilis dolorem quos quis magnam? Repellat illum cupiditate quam optio
+        laboriosam quibusdam ad ratione. Sapiente eaque qui accusamus
+        dignissimos nemo debitis alias esse veniam, incidunt ratione aut, et ab
+        dicta nulla magni mollitia. Eum at accusantium repellat quisquam dolor
+        ipsa suscipit possimus, maiores laborum tempore ab officia vero
+        architecto, ipsum magnam? Eos eveniet nostrum facere cum modi accusamus
+        obcaecati assumenda ea nam libero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum in
+        laudantium, id quis dicta nesciunt molestias ab esse vero accusamus
+        dolores rerum tempore totam, ullam, magnam harum pariatur nulla
+        voluptate accusantium animi error nam velit! Velit possimus quisquam,
+        beatae esse repudiandae earum voluptatum est nemo, temporibus quae,
+        reiciendis ipsa. Corrupti, nam ipsum non dicta fuga totam assumenda hic
+        veniam, maiores, corporis eos! Cumque deserunt commodi maxime cupiditate
+        necessitatibus rerum voluptatum repellat expedita fuga minima earum
+        veritatis dignissimos soluta consequatur, amet unde ipsam ab! Soluta
+        cupiditate et saepe, accusantium nemo nobis veritatis neque facilis nisi
+        voluptate ab numquam minus, magni illo.
+      </p>
+    </div>
+  );
+}
