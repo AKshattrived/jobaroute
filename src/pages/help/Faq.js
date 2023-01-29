3adc6462e86e183fa2ac508e2043c13993a6ca22
AKshattrived <akshattrivedi7901@gmail.com>
2023-01-29 18:26:44 +0530

    help page with nested routes

---
+++ src/pages/help/Faq.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+export default function Faq() {
+  return (
+    <div className="faq">
+      <h3>Frequently Asked Questions</h3>
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          neque, maxime iste suscipit quod repellat nostrum sapiente saepe!
+          Laudantium, aut! Fuga quae earum rerum. Aliquid perferendis voluptatem
+          sint eius quasi voluptas, earum ut voluptatibus ab eum, labore quis
+          odit ad sed dolor totam impedit. Rerum facilis earum voluptas quos
+          numquam.
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          neque, maxime iste suscipit quod repellat nostrum sapiente saepe!
+          Laudantium, aut! Fuga quae earum rerum. Aliquid perferendis voluptatem
+          sint eius quasi voluptas, earum ut voluptatibus ab eum, labore quis
+          odit ad sed dolor totam impedit. Rerum facilis earum voluptas quos
+          numquam.
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          neque, maxime iste suscipit quod repellat nostrum sapiente saepe!
+          Laudantium, aut! Fuga quae earum rerum. Aliquid perferendis voluptatem
+          sint eius quasi voluptas, earum ut voluptatibus ab eum, labore quis
+          odit ad sed dolor totam impedit. Rerum facilis earum voluptas quos
+          numquam.
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          neque, maxime iste suscipit quod repellat nostrum sapiente saepe!
+          Laudantium, aut! Fuga quae earum rerum. Aliquid perferendis voluptatem
+          sint eius quasi voluptas, earum ut voluptatibus ab eum, labore quis
+          odit ad sed dolor totam impedit. Rerum facilis earum voluptas quos
+          numquam.
+        </p>
+      </div>
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet?</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          neque, maxime iste suscipit quod repellat nostrum sapiente saepe!
+          Laudantium, aut! Fuga quae earum rerum. Aliquid perferendis voluptatem
+          sint eius quasi voluptas, earum ut voluptatibus ab eum, labore quis
+          odit ad sed dolor totam impedit. Rerum facilis earum voluptas quos
+          numquam.
+        </p>
+      </div>
+    </div>
+  );
+}
