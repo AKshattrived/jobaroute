506ca9cbf8767165b350637207c3ba16312e6cf5
AKshattrived <akshattrivedi7901@gmail.com>
2023-01-30 18:28:55 +0530

    Career Details with route parameters

---
+++ src/pages/careers/CareerDetails.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+
+export default function CareerDetails() {
+  const { id } = useParams();
+  const career = useLoaderData();
+
+  return (
+    <div className="career-details">
+      <h2>Career detail for {career.title}</h2>
+      <p>Starting salary: {career.salary}</p>
+      <p>Location: {career.location}</p>
+      <div className="details">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,
+          numquam laudantium obcaecati autem ipsam expedita quo neque et
+          temporibus reprehenderit, itaque fugit possimus, ducimus est odit
+          placeat assumenda quae voluptatibus?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export const careerDetailsLoader = async ({ params }) => {
+  const { id } = params;
+  const res = await fetch("http://localhost:4000/careers/" + id);
+  return res.json();
+};
